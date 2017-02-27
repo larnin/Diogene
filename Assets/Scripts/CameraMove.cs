6a0cae6ed0e3e376a82bc3a53c8539293686a1e1
Jean <jean.auquier@live.fr>
2017-02-27 18:02:10 +0100

    1er Test Fonctionnel
    Mise en évidence du besoin de jouer avec l'angle de la pente de l'escalier (voir 1rst et 2e scène).

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+	public float RoundVelocity = 1f;
+	public float DownVelocity = 1f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3(0, -DownVelocity * Time.deltaTime, 0));
+		transform.parent.transform.rotation *= Quaternion.Euler (0, RoundVelocity * Time.deltaTime, 0);
+	}
+}
