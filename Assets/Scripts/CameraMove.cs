d077afa300c9d3f7ed65d8dbe5cfb5a7af5bfc7d
Jean <jean.auquier@live.fr>
2017-02-27 19:16:59 +0100

    Proto jouable sur Android
    bouton back permet de reset, vitesse nécessite plus de tweek

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraMove : MonoBehaviour {
 
@@ -17,27 +18,35 @@ public class CameraMove : MonoBehaviour {
 		transform.Translate (new Vector3(0, -DownVelocity * Time.deltaTime, 0));
 		transform.parent.transform.rotation *= Quaternion.Euler (0, RoundVelocity * Time.deltaTime, 0);
 
-		if (Input.GetMouseButtonDown(0)) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-			Debug.DrawRay (ray.origin, ray.direction, Color.blue);
-			if (Physics.Raycast (ray, out hit, 1000, layerMask)) {
-				if (hit.collider.tag == "MasterRing") {
-					currentRing = hit.collider.transform;
-				}
-				else {
-					currentRing = hit.collider.transform.parent.transform;
-				}
+		var touches = Input.touches;
 
-				storedPosition = Input.mousePosition.x;
+		if (touches.Length > 0) {
+			if (touches[0].phase == TouchPhase.Began) {
+				Ray ray = Camera.main.ScreenPointToRay (touches[0].position);
+				RaycastHit hit;
+				Debug.DrawRay (ray.origin, ray.direction, Color.blue);
+				if (Physics.Raycast (ray, out hit, 1000, layerMask)) {
+					if (hit.collider.tag == "MasterRing") {
+						currentRing = hit.collider.transform;
+					}
+					else {
+						currentRing = hit.collider.transform.parent.transform;
+					}
+
+					storedPosition = touches[0].position.x;
+				}
+			}
+			else if (touches[0].phase == TouchPhase.Ended) {
+				currentRing = null;
+			}
+			else if (currentRing != null) {
+				currentRing.rotation *= Quaternion.Euler (0, -RingTurnSpeed * (touches[0].position.x - storedPosition) * Time.deltaTime, 0);
+				storedPosition = touches[0].position.x;
 			}
 		}
-		else if (Input.GetMouseButtonUp (0)) {
-			currentRing = null;
-		}
-		else if (Input.GetMouseButton (0) && (currentRing != null)) {
-			currentRing.rotation *= Quaternion.Euler (0, -RingTurnSpeed * (Input.mousePosition.x - storedPosition) * Time.deltaTime, 0);
-			storedPosition = Input.mousePosition.x;
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			SceneManager.LoadScene (0, LoadSceneMode.Single);
 		}
 	}
 }
