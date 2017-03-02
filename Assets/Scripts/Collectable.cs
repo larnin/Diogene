859c0b2f3023b8ee39978a2a9c5026097101a52f
Nicolas <nicolas-donald.laurent@laposte.net>
2017-03-02 20:45:29 +0100

    Pieces collectables

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class Collectable : MonoBehaviour
+{
+    public int value;
+
+    void Update()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
